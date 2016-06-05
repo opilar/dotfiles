f5b71762d29e63cab904f4aac86599a6092ab61a
opilarium <opilar@ya.ru>
2016-06-05 21:43:56 +0300

    EMACS!

---
+++ emacs/emacs.d.symlink/init.el
@@ -0,0 +1,9 @@
+(require 'package) 
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
