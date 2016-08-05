8acfac7dec29cf277c97b2e769ebb507a822043d
opilarium <opilar@ya.ru>
2016-08-05 22:55:08 +0300

    fuck vim. emacs rules

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -3,7 +3,14 @@
              '("melpa" . "https://melpa.org/packages/"))
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize)
+
+(add-to-list 'package-archives
+             '("elpy" . "https://jorgenschaefer.github.io/packages/"))
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
+(elpy-enable)
+(projectile-global-mode)
+
+(package-initialize)
