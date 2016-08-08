fb8bf0afc5ffbff0448a59a9e522bfdd79323071
opilarium <opilar@ya.ru>
2016-08-08 22:45:50 +0300

    emacs config

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -1,8 +1,6 @@
 (require 'package) 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-(when (< emacs-major-version 24)
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
 (add-to-list 'package-archives
              '("elpy" . "https://jorgenschaefer.github.io/packages/"))
@@ -10,7 +8,7 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-(elpy-enable)
+(require 'projectile)
 (projectile-global-mode)
 
 (package-initialize)
